70ae37c424260edcd5389ae0443bbd3bb0642b3b
Alejandra Suarez <mayerlysuarez1508@gmail.com>
2026-03-04 09:59:18 -0500

    Casos de pruebas

--- python_app/tests/test_lab.py
+++ python_app/tests/test_lab.py
@@ -1,11 +1,30 @@
-
-# TODO: Escribe pruebas unitarias para es_palindromo y suma.
-# Sugerencias:
-# - "radar" -> True, "anita lava la tina" -> True, "python" -> False, "" -> True, "Radar" -> True
-# - suma(2,3) -> 5; suma(0,5) -> 5; suma(-2,3) -> 1
-
+import pytest
 from palindromo import es_palindromo
 from utils import suma
 
-def test_ejemplo_siembra():
-    assert True
+# Pruebas para la función es_palindromo
+def test_palindromo_radar():
+    assert es_palindromo("radar") == True
+
+def test_palindromo_anita_lava_la_tina():
+    assert es_palindromo("anita lava la tina") == True
+
+def test_palindromo_python():
+    assert es_palindromo("python") == False
+
+def test_palindromo_vacio():
+    assert es_palindromo("") == True
+
+def test_palindromo_radar_mayusculas():
+    assert es_palindromo("Radar") == True
+
+
+# Pruebas para la función suma
+def test_suma_2_3():
+    assert suma(2, 3) == 5
+
+def test_suma_0_5():
+    assert suma(0, 5) == 5
+
+def test_suma_negativo_3():
+    assert suma(-2, 3) == 1
